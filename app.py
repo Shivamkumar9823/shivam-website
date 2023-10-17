612fd9defca2e6a63767f9ea48721000c81bacd6
Shivam <shivamkumarsingh.sk.2020@gmail.com>
2023-10-18 00:35:48 +0530

    updatae flask project

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+       {
+           'id' : 1,
+           'Title' : 'Data Analyst',
+           'Location':'Bengaluru, India',
+           'Salary' :'Rs 1000000'
+       },
+       {
+           'id' : 2,
+           'Title' : 'Data Scientist',
+           'Location':'Delhi, India',
+           'Salary' :'Rs 1000000'
+       },
+       {
+           'id' : 3,
+           'Title' : 'Backend Engineer',
+           'Location':'california',
+        #    'Salary' :'$ 12000'
+       },
+       {
+           'id' : 4,
+           'Title' : 'FrontEnd Engineer',
+           'Location':'New York',
+           'Salary' :'$100000'
+       } 
+
+]
+
+
 
 @app.route("/")
 
 def hello_world():
-    return "HELLO WORLD!"
+    return render_template('index.html',
+                           jobs=JOBS)
 
  
 if __name__ == '__main__':
