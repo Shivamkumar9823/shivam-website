ffcab08e6a818aee7ff5f27b88eb22fdf805b7e1
Shivam <shivamkumarsingh.sk.2020@gmail.com>
2023-10-19 00:26:54 +0530

    final chages dones

--- app.py
+++ app.py
@@ -7,25 +7,32 @@ JOBS = [
            'id' : 1,
            'Title' : 'Data Analyst',
            'Location':'Bengaluru, India',
-           'Salary' :'Rs 1000000'
+           'Salary' :'Rs 1000000',
+           'email':'dt2022@gmail.com'
        },
        {
            'id' : 2,
            'Title' : 'Data Scientist',
            'Location':'Delhi, India',
-           'Salary' :'Rs 1000000'
+           'Salary' :'Rs 1000000',
+           'email':'ds2022@gmail.com'
+
        },
        {
            'id' : 3,
            'Title' : 'Backend Engineer',
            'Location':'california',
         #    'Salary' :'$ 12000'
+           'email':'be2022@gmail.com'
+
        },
        {
            'id' : 4,
            'Title' : 'FrontEnd Engineer',
            'Location':'New York',
-           'Salary' :'$100000'
+           'Salary' :'$100000',
+           'email':'fe2022@gmail.com'
+
        } 
 
 ]
